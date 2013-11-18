4471ecaea10d276c22c41fa94288a13f2646cb6b
Radosław Sporny <r.sporny@gmail.com>
2013-11-18 22:37:40 +0100

    add: unit test for getUserByLogin

---
+++ src/test/java/br/eti/kinoshita/testlinkjavaapi/user/TestGetUserByLogin.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) <2010> <Bruno P. Kinoshita>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.eti.kinoshita.testlinkjavaapi.user;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import br.eti.kinoshita.testlinkjavaapi.BaseTest;
+import br.eti.kinoshita.testlinkjavaapi.model.User;
+import br.eti.kinoshita.testlinkjavaapi.util.TestLinkAPIException;
+
+/**
+ * @author Radoslaw Sporny
+ * 
+ */
+public class TestGetUserByLogin extends BaseTest {
+
+	@DataProvider(name = "userData")
+	public Object[][] createData() {
+		return new Object[][] { { "jan.kowalski" } };
+	}
+
+	@Test(dataProvider = "userData")
+	public void testGetUserByLogin(String login) {
+		this.loadXMLRPCMockData("tl.getUserByLogin.xml");
+
+		User user = null;
+
+		try {
+			user = api.getUserByLogin(login);
+		} catch (TestLinkAPIException e) {
+			Assert.fail(e.getMessage(), e);
+		}
+
+		Assert.assertNotNull(user);
+
+		Assert.assertTrue(user.getDbID() > 0);
+
+		Assert.assertTrue(user.getUserApiKey().isEmpty());
+	}
+
+}
